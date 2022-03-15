4e5a94b9839d465a01a914f78bbc6d964632b1dc
Lonexw <lone@yousails.com>
2022-03-15 18:22:40 +0800

    change typescript dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 // rollup.config.js
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
